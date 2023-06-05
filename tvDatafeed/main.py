1bfe4bb13198ea8e39b4a6adb5b5ca1c15963ae3
Nusoma <1662820+ratanlp@users.noreply.github.com>
2023-06-05 11:48:04 +0530

    # Update main.py. Added in_75_minute = "75"

--- tvDatafeed/main.py
+++ tvDatafeed/main.py
@@ -24,6 +24,7 @@ class Interval(enum.Enum):
     in_15_minute = "15"
     in_30_minute = "30"
     in_45_minute = "45"
+    in_75_minute = "75"
     in_1_hour = "1H"
     in_2_hour = "2H"
     in_3_hour = "3H"
